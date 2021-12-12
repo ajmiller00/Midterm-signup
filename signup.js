302553b5e6b8b4cb48ff8b11a8fc56e71b1ca234
ajmiller00 <47611503+ajmiller00@users.noreply.github.com>
2021-12-12 17:16:34 -0500

    Update signup.js

--- signup.js
+++ signup.js
@@ -4,7 +4,7 @@ var qs = require('querystring');
 const express = require('express');
 const app = express();
 //var auth = require('https://ajmiller00.github.io/Midterm-signup/logIn.js');
-var add = require('https://ajmiller00.github.io/Midterm-signup/addUser.js');
+//var add = require('https://ajmiller00.github.io/Midterm-signup/addUser.js');
 
 var port = process.env.PORT || 3000;
 // var port = 8080;
@@ -65,7 +65,7 @@ app.post('/addUser', async (req, res) => {
 	   pdata = qs.parse(pdata);
 	   // res.write ("The email is: " + pdata['email'] + "<br>");
 	   // res.write ("The password is: " + pdata['password'] + "<br>");
-	   const result = await add.addUser(pdata);
+	   const result = "";
 	   res.writeHead(200, {'Content-Type':'text/html'});
 		res.write("<!DOCTYPE html><html lang='en'><head><script src='https://code.jquery.com/jquery-3.6.0.min.js' integrity='sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=' crossorigin='anonymous'>");
 		res.write("</script><meta charset='UTF-8'><meta name='viewpor' content='width=device-width, initial-scale=1.0'><link rel='preconnect' href='https://fonts.googleapis.com'>");
